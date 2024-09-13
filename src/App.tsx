3c82989eebbfd7394f3603e811a085446115d95d
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-09-13 20:25:05 -0300

    chore: add styles to application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <p>Hello World!</p>;
+  return <h1>Hello World!</h1>;
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6C25',
+        primary50: 'rgba(255, 108, 37, 0.5)',
+
+        secondary: '#FFF',
+        secondary70: 'rgba(255, 255, 255, 0.7)',
+
+        text: '#000',
+        overlay40: 'rgba(0, 0, 0, 0.4)',
+        background: '#151515',
+      },
+
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+
+      boxShadow: {
+        default:
+          '0px 0px 24px 0px rgba(255, 108, 37, 0.5), 0px 0px 24px 0px rgba(255, 108, 37, 0.5)',
+      },
+    },
+  },
+  plugins: [require('tailwind-scrollbar')],
+};
