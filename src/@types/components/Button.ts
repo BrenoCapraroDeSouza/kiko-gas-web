6796b14aabfb3bc56228b15008d2ab4c2112904d
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-09-23 20:23:13 -0300

    feat: add secondary variant to the `Button` component

--- src/@types/components/Button.ts
+++ src/@types/components/Button.ts
@@ -1,7 +1,10 @@
+export type ButtonVariant = 'primary' | 'secondary';
+
 export type ButtonType = 'button' | 'submit';
 
 export interface ButtonProps {
   title: string;
+  variant?: ButtonVariant;
   type?: ButtonType;
   isDisabled?: boolean;
   isHugWidth?: boolean;
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,6 @@
 import { memo } from 'react';
 
-import { ButtonProps } from '@/@types';
+import { ButtonProps, ButtonVariant } from '@/@types';
 
 import { Spinner, Text } from '..';
 
@@ -10,24 +10,35 @@ function Button(props: ButtonProps) {
     isDisabled = false,
     isHugWidth = false,
     isLoading = false,
+    variant = 'primary',
     type = 'button',
     onClick,
   } = props;
 
+  const isDisabledOrLoading = isDisabled || isLoading;
+  const isPrimary = variant === 'primary';
   const width = isHugWidth ? 'w-full' : 'w-auto';
-  const cursor = isDisabled ? 'cursor-not-allowed' : 'cursor-pointer';
-  const backgroundColor = isDisabled ? 'bg-primary50' : 'bg-primary';
+  const cursor = isDisabledOrLoading ? 'cursor-not-allowed' : 'cursor-pointer';
   const textColor = isDisabled ? 'secondary70' : 'secondary';
+  const shadow = isPrimary ? 'shadow-default' : '';
+  const border = !isPrimary
+    ? 'border border-secondary disabled:border-secondary70'
+    : '';
+
+  const backgroundColors: Record<ButtonVariant, string> = {
+    primary: isDisabled ? 'bg-primary50' : 'bg-primary',
+    secondary: 'bg-content',
+  };
 
   return (
     <button
-      className={`flex ${width} h-15 justify-center items-center px-4 ${backgroundColor} rounded ${cursor} shadow-default disabled:shadow-none hover:opacity-90 transition-colors duration-300 disabled:hover:opacity-100 focus-visible:outline-none focus-visible:ring focus-visible:ring-secondary`}
+      className={`flex ${width} h-15 justify-center items-center px-4 ${backgroundColors[variant]} ${border} rounded ${cursor} ${shadow} disabled:shadow-none hover:opacity-90 transition-colors duration-300 disabled:hover:opacity-100 focus-visible:outline-none focus-visible:ring focus-visible:ring-secondary`}
       type={type}
-      disabled={isDisabled}
+      disabled={isDisabledOrLoading}
       onClick={onClick}
     >
       {isLoading ? (
-        <Spinner />
+        <Spinner color={textColor} />
       ) : (
         <Text color={textColor} size='alternative' weight='semibold'>
           {title}
