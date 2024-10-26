2d999b734cc15f3ee5e9a3de2575bd4fd54b6135
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-10-26 18:46:11 -0300

    feat: add `title` to the `CloseModalButton` component

--- src/components/CloseModalButton/index.tsx
+++ src/components/CloseModalButton/index.tsx
@@ -10,6 +10,7 @@ function CloseModalButton(props: CloseModalButtonProps) {
   return (
     <button
       type='button'
+      title='Fechar'
       className='absolute flex top-3 right-3 justify-center items-center size-8 bg-primary rounded-full cursor-pointer shadow-default disabled:bg-primary50 disabled:hover:opacity-100 disabled:shadow-none disabled:cursor-not-allowed hover:opacity-90 transition-colors duration-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-secondary focus-visible:border-secondary'
       disabled={isLoading}
       onClick={onClose}
